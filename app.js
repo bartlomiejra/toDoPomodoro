7633f68efef1377d94d6969fc93af2c0256836c5
Bart <bartlomiej.ra@gmail.com>
2020-09-08 20:35:15 +0200

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+/*
+* Importand Information
+! Deprecated method, do not use
+? should this method be exposad in the public API
+TODO: zrobić to i tamto
+* @param myParam The parameter for this method
+
+
+
+
+
+*/
