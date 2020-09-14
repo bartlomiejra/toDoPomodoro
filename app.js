fc9c3e237ac882f48fe2d0a6ef3b096aa93f6200
Bart <bartlomiej.ra@gmail.com>
2020-09-14 20:43:23 +0200

    styling

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const todoInput = document.querySelector(".todoInput");
 const todoButton = document.querySelector(".addtaskButton");
 const todoList = document.querySelector(".todolist");
@@ -11,29 +9,36 @@ const todoList = document.querySelector(".todolist");
 
 
 //* this function adding new item todo
-function addTodo(event){
-event.preventDefault();
-console.log("addtask");
+function addTodo(event) {
+    event.preventDefault();
+    console.log("addtask");
+    const task = document.createElement("div");
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "Hey Pomodorooo";
+    newTodo.classList.add('todo-item');
+    task.appendChild(newTodo);
 
-const task = document.createElement("task");
-const newTodo = document.createElement('li');
-newTodo.innerText ="Hey Pomodorooo";
-newTodo.classList.add('todo-item');
-task.appendChild(newTodo);
-//* Adding Completed button to new item task
-const completedButton = document.createElement('button');
- completedButton.innerHTML= '<i class="fas fa-check"></i>';
-completedButton.classList.add("complete-btn");
-task.appendChild(completedButton);
+    //* Adding Completed button to new item task
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-square"></i>';
+    completedButton.classList.add("complete-btn");
+    task.appendChild(completedButton);
 
-//* Add Delate button to new item task
-const delatedButton = document.createElement('button');
-delatedButton.innerHTML= '<i class="fas fa-trash"></i>';
-delatedButton.classList.add("complete-btn");
-task.appendChild(delatedButton);
+    //* Add Delate button to new item task
+    const delatedButton = document.createElement('button');
+    delatedButton.innerHTML = '<i class="fas fa-trash"></i>';
+    delatedButton.classList.add("delete-btn");
+    task.appendChild(delatedButton);
 
-//* Apend item to list ➕
-todoList.appendChild(task);
+
+    //* Add startTimer button
+    const startTimerButton = document.createElement('button');
+    startTimerButton.innerHTML = '<i class="fas fa-play"><i>';
+    startTimerButton.classList.add("play-btn");
+    task.appendChild(startTimerButton);
+
+    //* Apend item to list ➕
+    todoList.appendChild(task);
 
 }
 
