6f083b286216e3382cb8465d5efe26a9edb9cb89
Bart <bartlomiej.ra@gmail.com>
2020-09-18 11:23:13 +0200

    asd

--- app.js
+++ app.js
@@ -2,57 +2,51 @@ const todoInput = document.querySelector(".todoInput");
 const todoButton = document.querySelector(".addtaskButton");
 const todoList = document.querySelector(".todolist");
 
-
-
-
-
-
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", delatedTask);
 
 //* this function adding new item todo
 function addTodo(event) {
-    event.preventDefault();
-    console.log("addtask");
-    const task = document.createElement("div");
-    const newTodo = document.createElement('li');
-    newTodo.innerText = "Hey Pomodorooo";
-    newTodo.classList.add('todo-item');
-    task.appendChild(newTodo);
+  event.preventDefault();
+  console.log("addtask");
+  const task = document.createElement("div");
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  task.appendChild(newTodo);
 
-    //* Adding Completed button to new item task
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-square"></i>';
-    completedButton.classList.add("complete-btn");
-    task.appendChild(completedButton);
+  //* Adding Completed button to new item task
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-circle"></i>';
+  completedButton.classList.add("complete-btn");
+  task.appendChild(completedButton);
 
-    //* Add Delate button to new item task
-    const delatedButton = document.createElement('button');
-    delatedButton.innerHTML = '<i class="fas fa-trash"></i>';
-    delatedButton.classList.add("delete-btn");
-    task.appendChild(delatedButton);
+  //* Add Delate button to new item task
+  const delatedButton = document.createElement("button");
+  delatedButton.innerHTML = '<i class="fas fa-trash"></i>';
+  delatedButton.classList.add("delete-btn");
+  task.appendChild(delatedButton);
 
+  //* Add startTimer button
+  const startTimerButton = document.createElement("button");
+  startTimerButton.innerHTML = '<i class="fas fa-play"><i>';
+  startTimerButton.classList.add("play-btn");
+  task.appendChild(startTimerButton);
 
-    //* Add startTimer button
-    const startTimerButton = document.createElement('button');
-    startTimerButton.innerHTML = '<i class="fas fa-play"><i>';
-    startTimerButton.classList.add("play-btn");
-    task.appendChild(startTimerButton);
+  //* Apend item to list ➕
+  todoList.appendChild(task);
 
-    //* Apend item to list ➕
-    todoList.appendChild(task);
-
+  //*Clear input lebel to
+  todoInput.value = "";
 }
 
-
-
-
-
-
-todoButton.addEventListener('click', addTodo);
-
-
-
-
-
+function delatedTask(e) {
+  const item = e.target;
+  //DELATE
+  if (item.classList[0] === "delete-btn") {
+    item.remove();
+  }
+}
 
 /*
 * Importand Information
