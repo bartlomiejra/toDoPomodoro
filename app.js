5a17122344ce046b9c0ddaddb80cc4955172bc7f
Bart <bartlomiej.ra@gmail.com>
2021-02-15 18:28:26 +0100

    after add event listener to timerbreak

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const clockTimer = document.querySelector('.clock');
 const buttonscountdown = document.querySelector('.countdownButtons');
 const pause = document.querySelector('.pauseButton');
 const reset = document.querySelector('.resetButton');
+const breakdown = document.querySelector('.fa-coffee');
+
 // const task = document.querySelector('divT');
 // const newTodo = document.querySelector('.todo-item');
 
@@ -13,9 +15,12 @@ const todos = JSON.parse(localStorage.getItem('Items')) || [];
 // virable to audio file
 let audio;
 // virable to break time (5 minut defoult)
-const shortBreak = 10;
 
 let timeInFocus;
+
+let countdownTime;
+const countdownTimer = document.getElementById('countdown');
+let taskId = 0;
 
 //* TODO Rebuind addToDO add new function to create structur of task element like button etc.
 
@@ -45,6 +50,7 @@ ${
     )
     .join('');
 }
+
 // function play() {
 //   var audio = new Audio(
 //     'https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3',
@@ -52,13 +58,16 @@ ${
 //   audio.play();
 // }
 
+//
 function addTodo(event) {
   event.preventDefault();
+
   const item = {
     text: todoInput.value,
     done: false,
     focus: 0,
   };
+  // push and add task to localstorage
   todos.push(item);
   lists(todos, todoList);
   localStorage.setItem('Items', JSON.stringify(todos));
@@ -66,9 +75,10 @@ function addTodo(event) {
 }
 
 function statTask() {
-  //* Wyliczanie statystyk czasu i ukończonych tasków
   let toBeCompleted = 0;
   let countCompleted = 0;
+
+  //* counting statiscics
   for (let i = 0; i < todos.length; i += 1) {
     if (todos[i].done === true) {
       countCompleted += 1;
@@ -76,6 +86,7 @@ function statTask() {
       toBeCompleted += 1;
     }
   }
+  // let totalfocustime = 0;
   const pomodoreDuration = 25;
   document.getElementById('completedTasks').innerHTML = countCompleted;
   document.getElementById('taskstobe').innerHTML = toBeCompleted;
@@ -115,10 +126,6 @@ todoButton.addEventListener('click', addTodo);
 lists(todos, todoList);
 
 //* functions buttons action delate play and completted task
-
-let countdownTime;
-const countdownTimer = document.getElementById('countdown');
-let taskId = 0;
 
 function btnActtion(e) {
   statTask();
@@ -167,6 +174,7 @@ function btnActtion(e) {
 
   //* timer start function
   if (item.classList[0] === 'play-btn') {
+    resetTimer();
     item.innerHTML = '<i class="fa fa-clock"></i>';
     // myAudio.pause();
     const { index } = e.target.id;
@@ -174,7 +182,7 @@ function btnActtion(e) {
     timer();
 
     clearInterval(countdownTime);
-    const seconds = 10;
+    const seconds = 4;
     timer(seconds);
 
     clockTimer.classList.remove('timerFinish');
@@ -196,68 +204,154 @@ function displayTimeLeft(seconds) {
   countdownTimer.textContent = display;
   document.title = display;
 }
+let secondsLeft;
+let paused = false; // is the clock paused?
 
 function timer(seconds) {
+  pause.firstElementChild.classList.remove('fa-coffee');
+  pause.firstElementChild.classList.add('fa-pause');
+  pause.addEventListener('click', pausetimer);
   // after start timer clear any exsisting timers
   clearInterval(countdownTime);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   countdownTime = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    secondsLeft = Math.round((then - Date.now()) / 1000);
     timeInFocus = seconds - secondsLeft;
     if (secondsLeft < 0) {
       endpomodoro();
+      clearInterval(countdownTime);
       return;
     }
 
-    let paused = false; // is the clock paused?
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+const shortBreak = 5;
 
-    function pausetimer() {
-      const secsave = secondsLeft;
-      if (!paused) {
-        paused = true;
-        this.firstElementChild.classList.remove('fa-pause');
-        this.firstElementChild.classList.add('fa-play');
-        clearInterval(countdownTime);
-      } else {
-        this.firstElementChild.classList.remove('fa-play');
-        this.firstElementChild.classList.add('fa-pause');
-        paused = false;
-        displayTimeLeft(secsave);
-        timer(secsave);
-      }
-    }
+function timerBreak() {
+  pause.firstElementChild.classList.remove(
+    'fa-play',
+    'fa-coffee',
+    'fa-pause-circle',
+    'fa-pause',
+  );
+  // pause.firstElementChild.classList.add('');
+  console.log('timerbreakstart');
 
-    function resetTimer() {
-      clockTimer.classList.remove('timerFinish', 'timerStart');
-      lists(todos, todoList);
-      buttonscountdown.classList.add('countdownButtonsNone');
+  // pause.addEventListener('click', pausetimer);
+  // after start timer clear any exsisting timers
+  clearInterval(countdownTime);
+  const now = Date.now();
+  const then = now + shortBreak * 1000;
+  displayTimeLeft(shortBreak);
+  countdownTime = setInterval(() => {
+    secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      audio = new Audio('Alerts/pauseEnd.mp3');
+      audio.play();
+      resetTimer();
       clearInterval(countdownTime);
-      displayTimeLeft(0);
-      // }
+
+      return;
     }
 
-    pause.addEventListener('click', pausetimer);
-    reset.addEventListener('click', resetTimer);
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
-//* funkcja lists wczytująca taski z localstore
-lists(todos, todoList);
+// function pausetimer() {
+//   console.log('pause funkcja');
+//   const secsave = secondsLeft;
 
-todoList.addEventListener('click', btnActtion);
+//   if (this.firstElementChild.classList.contains('fa-pause')) {
+//     console.log('było pause');
+//     this.firstElementChild.classList.remove('fa-pause');
+//     this.firstElementChild.classList.add('fa-play');
+//     clearInterval(countdownTime);
+//   } else {
+//     console.log('było play');
+//     this.firstElementChild.classList.remove('fa-play');
+//     this.firstElementChild.classList.add('fa-pause');
+//     paused = false;
+//   }
 
-function breakTime() {
-  clearInterval(countdownTime);
-  clockTimer.classList.add('timerFinish');
-  clockTimer.classList.remove('timerStart');
+//   displayTimeLeft(secsave);
+//   timer(secsave);
+// }
+
+function pausetimer() {
+  // console.log(setinterval);
+  // console.log(pause);
+  const secsave = secondsLeft;
+  // console.log('secsave :>> ', secsave);
+  if (!paused) {
+    paused = true;
+    // console.log('zatrzymano timer');
+    // console.log();
+    this.firstElementChild.classList.remove('fa-pause');
+    this.firstElementChild.classList.add('fa-play');
+    // console.log('pausa');
+    // console.log('.pauseButton');
+    // this.firstElementChild.ClassList.add('fa-play');
+    // console.dir(buttonscountdown);
+    // this.firstElementChild.innerHTML = '';
+    // this.firstElementChild.innerHTML = '<i class="fa fa-play"></i>';
+
+    clearInterval(countdownTime);
+  } else {
+    this.firstElementChild.classList.remove('fa-play');
+    this.firstElementChild.classList.add('fa-pause');
+    // console.log('wznowiono timer');
+    paused = false;
+    displayTimeLeft(secsave);
+    timer(secsave);
+    // console.log(secsave);
+    // console.log(this);
+
+    // this.firstElementChild.ClassList.add('fa-play');
+  }
+  // https://codepen.io/yaphi1/pen/QbzrQP
+}
+
+function resetTimer() {
+  clockTimer.classList.remove('timerFinish', 'timerStart');
   lists(todos, todoList);
   buttonscountdown.classList.add('countdownButtonsNone');
-  displayTimeLeft(shortBreak);
+  clearInterval(countdownTime);
+  displayTimeLeft(0);
+  // }
+}
 
-  // timer(shortBreak);w
+//* funkcja lists wczytująca taski z localstore
+
+function breakTime() {
+  console.log('funkcjia pausa');
+  clearInterval(countdownTime);
+  clockTimer.classList.add('timerFinish');
+  console.log(pause);
+  pause.firstElementChild.classList.remove(
+    'fa-play',
+    'fa-pause',
+    'fa-pause-circle',
+  );
+
+  pause.firstElementChild.classList.add('fa-coffee');
+  // pause.classList.remove('fa-pause');
+  // pause.classList.add('fa-play');
+  // clockTimer.secondElementChild
+  // clockTimer.firstElementChild.classList.add('fa-play');
+  clockTimer.classList.remove('timerStart');
+  lists(todos, todoList);
+
+  // buttonscountdown.classList.add('countdownButtonsNone');
+  // displayTimeLeft(shortBreak);
+
+  // timerBreak();
+  // timerBreak();
+
+  // timer(shortBreak);
   //   clearInterval(countdownTime);
   //   audio = new Audio('Alerts/pauseEnd.mp3');
   //   audio.play();
@@ -270,10 +364,20 @@ function endpomodoro() {
   const itemS = JSON.parse(localStorage.getItem('Items'));
   itemS[taskId].focus += timeInFocus;
   localStorage.setItem('Items', JSON.stringify(itemS));
+  // paused = true;
+  // this.firstElementChild.classList.remove('fa-pause');
+  // this.firstElementChild.classList.add('fa-play');
+  lists(todos, todoList);
+  pause.addEventListener('click', timerBreak);
 
   breakTime();
-}
 
+  // timer();
+}
+pause.addEventListener('click', pausetimer);
+reset.addEventListener('click', resetTimer);
+todoList.addEventListener('click', btnActtion);
+lists(todos, todoList);
 /*
   * Importand Information
   ! Deprecated method, do not use
