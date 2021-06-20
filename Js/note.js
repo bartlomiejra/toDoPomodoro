a3bc0369275e61215cdb6bfafc7387423075fe99
bartlomiejra <bartlomiej.ra@gmail.com>
2021-06-20 22:32:51 +0200

    notecard

--- Js/note.js
+++ Js/note.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-undef */
 const openNote = document.getElementById('noteButton');
 const closeNote = document.getElementById('closeNotebtn');
+const closeoneNote = document.getElementById('closethisNote');
 const noteCard = document.querySelector('.noteCard');
+const deleteNote = document.getElementById('deleteNote');
+const noteHeader = document.getElementById('noteHeader');
+const noteText = document.getElementById('textarea');
 
 function openNoteCard() {
       noteCard.classList.add('active');
@@ -14,3 +18,38 @@ function closeNoteCard() {
 }
 openNote.addEventListener('click', openNoteCard);
 closeNote.addEventListener('click', closeNoteCard);
+
+const createNote = document.querySelector('.tiles__add');
+const onenoteCard = document.querySelector('.tiles__note');
+
+function addNote() {
+      console.log('oki działam');
+      onenoteCard.classList.remove('none');
+      noteCard.classList.add('overlayNote');
+}
+
+function closeonenoteCard() {
+      console.log('oki działam');
+      onenoteCard.classList.add('none');
+      noteCard.classList.remove('overlayNote');
+}
+
+createNote.addEventListener('click', addNote);
+closeoneNote.addEventListener('click', closeonenoteCard);
+
+const saveNote = document.getElementById('saveNote');
+
+saveNote.addEventListener('click', () => {
+      console.log('saveNote');
+      const person = {
+            title: noteHeader.value,
+            note: noteText.value,
+      };
+      window.localStorage.setItem('user', JSON.stringify(person));
+});
+
+function deleteyourNote() {
+      console.log('deleteNote');
+}
+
+deleteNote.addEventListener('click', deleteyourNote);
