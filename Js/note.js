c0355fafe6f59b7aa1449ca60652858defee5a72
bartlomiejra <bartlomiej.ra@gmail.com>
2021-06-30 20:01:13 +0200

    ASD

--- Js/note.js
+++ Js/note.js
@@ -29,45 +29,42 @@ const onenoteCard = document.querySelector('.tiles__note');
 function addNote() {
       console.log(tilesNote);
       tilesNote.innerHTML = `
-        	  <button class="tiles__savebtn" onClick='saveNote()' id="saveNote">Save</button>
+        	  <button class="tiles__savebtn" onClick="saveNote()"id="saveNote">Save</button>
         	  <button class="tiles__deletebtn closeNotebtn" onClick="deleteyourNote()" id="deleteNote">Delete Last Note</button>
-        	  <button  id="closethisNote" class="modal__closebutton onClick='closeonenoteCard()'"><i class="fas fa-times"></i></button>
+        	  <button  id="closethisNote" class="modal__closebutton" onClick='closeonenoteCard()'><i class="fas fa-times"></i></button>
         	  <input class="tiles__noteHeader" id="noteHeader" placeholder="Title" type="text">
         	  <p class="date" value=dateToday>
 			  ${dateToday} 
 			 
 			  </p>
-        	  <input type="textarea" rows="2" cols="20" wrap="hard" id="textarea" placeholder="Type something... &#13;" class="tiles__textarea">
+        	  <input type="textarea" rows="2" cols="20" wrap="hard"  type="textarea" id="textarea" placeholder="Type something... &#13;" class="tiles__textarea">
 
           `;
 
-      console.log('oki działam');
       onenoteCard.classList.remove('none');
       noteCard.classList.add('overlayNote');
 
-      const noteHeader = document.getElementById('noteHeader');
-      const noteText = document.getElementById('textarea');
-      saveNote(noteText);
-
-      function saveNote(e) {
-            event.preventDefault();
-
-            console.log('saveNote');
-            const person = {
-                  title: noteHeader.value,
-                  date: dateToday,
-                  note: noteText.value,
-            };
-            notes.push(person);
-
-            window.localStorage.setItem('Notes', JSON.stringify(notes));
-            notesrender();
-      }
+      //   saveNote(noteText);
 }
 
+function saveNote() {
+      // event.preventDefault();
+
+      const noteHeader = document.getElementById('noteHeader');
+      const noteText = document.getElementById('textarea');
+      console.log('saveNote');
+      const person = {
+            id: notes.length,
+            title: noteHeader.value,
+            date: dateToday,
+            note: noteText.value,
+      };
+      notes.push(person);
+      window.localStorage.setItem('Notes', JSON.stringify(notes));
+      notesrender();
+}
 function closeonenoteCard() {
-      console.log('oki działam');
-      onenoteCard.classList.add('none');
+      tilesNote.classList.add('none');
       noteCard.classList.remove('overlayNote');
 }
 
@@ -75,30 +72,19 @@ function closeSingleNote() {
       onenoteCard.classList.add('none');
       noteCard.classList.remove('overlayNote');
 }
-
 createNote.addEventListener('click', addNote);
-
-// const saveNote = document.getElementById('saveNote');
-
-function deleteyourNotes() {
-      console.log('deleteNote');
-}
-
 function deleteyourNote() {
+      console.log('delete Last Note');
       notes.pop();
       window.localStorage.setItem('Notes', JSON.stringify(notes));
-
       console.log('deleteNote');
 }
-
-// deleteNote.addEventListener('click', deleteyourNote);
 
 function notesrender() {
       tiles.innerHTML = notes
             .map(
                   (note, i) => `
-
-				  <div class="tiles__tile" value=${note.title}  id="${i}"onClick="noteOpen(this.id)">
+				  <div class="tiles__tile" value=${note.title}  id="${i}" onClick='noteOpen(this.id)'>
 				<h3>${note.title}</h3>
 					
 				<div class="tiles__date">${note.date} </div>
@@ -113,23 +99,21 @@ function notesrender() {
 notesrender();
 
 let click;
-function noteOpen(clicked_id) {
-      notes = JSON.parse(localStorage.getItem('Notes')) || [];
 
-      addNote();
+function noteOpen(clicked_id) {
+      console.log(clicked_id);
+      notes = JSON.parse(localStorage.getItem('Notes')) || [];
+      onenoteCard.classList.remove('none');
       onenoteCard.innerHTML = `
       <button class="tiles__savebtn" id="saveNote" onClick='saveEditNote()'>Save</button>
 					<button class="tiles__deletebtn closeNotebtn" id="deleteNotes" onClick='removeNote()'>Delete Note</button>
 					<button   onclick='closeSingleNote()' class="modal__closebutton "><i class="fas fa-times"></i></button>
 					<input class="tiles__noteHeader headerNote" id="noteHeader" value=${notes[clicked_id].title} type="text">
-					<p class="date" value=${notes[clicked_id].date}></p>
-					<input type="textarea" rows="2" cols="20" wrap="hard" id="textarea" placeholder="Type something... &#13;" class="tiles__textarea notetext" value=${notes[clicked_id].note} >
+					<p class="date" value=${notes[clicked_id].date}>${notes[clicked_id].date}</p>
+					<input type="textarea" wrap="hard" id="textarea" placeholder="Type something... &#13;" class="tiles__textarea notetext" value=${notes[clicked_id].note} >
 
       `;
-
-      console.log(clicked_id);
       click = clicked_id;
-      console.log(notes[clicked_id].title);
 }
 function saveEditNote() {
       notes = JSON.parse(localStorage.getItem('Notes')) || [];
@@ -137,9 +121,7 @@ function saveEditNote() {
       notetext = document.querySelector('.notetext');
       notes[click].title = headerN.value;
       notes[click].note = notetext.value;
-
       localStorage.setItem('Notes', JSON.stringify(notes));
-
       notesrender();
 }
 
@@ -152,4 +134,5 @@ function removeNote() {
       }
       localStorage.setItem('Notes', JSON.stringify(notes));
       notesrender();
+      onenoteCard.classList.add('none');
 }
