9611a0beb2bbec0543b277bd76bac5e0669790fc
bartlomiejra <bartlomiej.ra@gmail.com>
2021-09-20 16:13:33 +0200

    in progres

--- Js/todo.js
+++ Js/todo.js
@@ -1,5 +1,7 @@
 let last = 0;
 const DivToDo = document.querySelector('.todo_List');
+// const CategoryTo = document.querySelector('.dropcategory');
+
 const divCategory = document.querySelector('.todo_category');
 const todoTitle = document.querySelector('.todo_input');
 const categoryTitle = document.querySelector('.category_input');
@@ -8,24 +10,41 @@ const openPopup = document.querySelector('.openpopup')
 const menugui = document.getElementsByClassName('gui-popup')
 const todoCard = document.querySelector('.todoCard');
 const taskCategory = document.querySelector('.taskCategory')
-const item = document.querySelector('.item')
-const box = document.querySelector('.box')
-const newtoDoBox = document.querySelector(".addTodo");
+const itemTodos = document.querySelector('.itemTodos')
 //!Empty State
-
+let Position = [];
 var categories = []
-let Position = JSON.parse(localStorage.getItem('Position'));
-if (Position == null) {
-	Position = [
-		{
-	  id: 0,
-	  text: 'First',
-	  done: false,
-	  category: 'inbox',
-	  data: '30.1.21',
-	},
-];
+ PositionList = JSON.parse(localStorage.getItem('Position'));
+ console.log(PositionList);
+ if (PositionList == null) {
+	 console.log('ok');
+
+	 
+	 
+
+	PositionList = [{
+		task: 1,
+			div: 1
+	}];
+			
+	// 	 newList = [{
+
+	// 		task: thisTask,
+	// 		div: thisBox
+	// 	}
+	// ]
+	// ;
+	
+	
 }
+console.log(Position);
+
+Position.push(PositionList);
+	console.log(Position);
+	localStorage.setItem('Position', JSON.stringify(PositionList));
+	 PositionList = JSON.parse(localStorage.getItem('Position'));
+
+
 
 
 
@@ -91,7 +110,7 @@ function addNewTodo(event) {
 	};
 	//   todoInput.value = '';
 	ListOfToDo.push(ToDoNew);
-	console.log(ListOfToDo);
+	// console.log(ListOfToDo);
 	localStorage.setItem('ListTodo', JSON.stringify(ListOfToDo));
 
 	todoTitle.value = '';
@@ -107,6 +126,7 @@ renderTodos();
 
 
 function renderTodos() {
+
 	ListOfToDo = JSON.parse(localStorage.getItem('ListTodo'));
 console.log(ListOfToDo)
 	console.log('render');
@@ -117,14 +137,15 @@ console.log(ListOfToDo)
 // )
 // let p = document.createElement("p")
 // divCategory.append(p)
-DivToDo.innerHTML = ListOfToDo.map(
+// CategoryTo
+itemTodos.innerHTML = ListOfToDo.map(
 		(todo) =>`
 		
 
 		
 		
 
-			<div class="todo_item dragitem drag-drop" id=${todo.id} ">
+			<div class="todo_item dragitem" id=${todo.id} virable=${todo.category} draggable="true">
 			
 			<input type="checkbox">
 			${todo.text}
@@ -145,7 +166,7 @@ DivToDo.innerHTML = ListOfToDo.map(
 		)
 		.join('');
 	}
-// dragAndDrop();
+dragAndDrop();
 
 
 
@@ -186,7 +207,7 @@ function renderCategory() {
 	
 	divCategory.innerHTML = ListOfCategory.map(
 		(cate) => `
-		<div id="inner-dropzone" class="  todo_categoryBox dropzone dropcategory" value="${cate.text}"  style="background-color: ${cate.color};" >  
+		<div class="  todo_categoryBox " id="${cate.text}" value="${cate.text}"  style="background-color: ${cate.color};" >  
 		<div class="todo_categoryTittle">
 		
 		
@@ -195,102 +216,165 @@ function renderCategory() {
 		<button  onClick="deleteCategory(this)" autofocus=""data-index=${cate.id} id=${cate.id} class="todo_categoryRemove" aria-label="Delete">
 		<i class="fas fa-minus-circle dltbtn" aria-hidden="true"> </i>
 		</button>
+		
+
 		</div>
+		<div class="box">
+		</div>
+		
 		</div>
 		</div>`,
 		)
 		.join('');
-		// dragAndDrop();
+		dragAndDrop();
 	}
 	renderCategory();
 	
 	let	taskplace = document.querySelector('taskplace');
 
-	item.innerHTML = `
-	<div id="no-drop" class="drag-drop dragitem "> #no-drop </div>
+
+	// drag and drop inplement
+	function dragAndDrop(){
+
+		
+		const dragItems = document.querySelectorAll('.dragitem');
+		const category = document.querySelectorAll('.dropcategory');
+	
+		let draggedItem = null;
+		
+		for (let i = 0; i < dragItems.length; i++) {
+			const item = dragItems[i];		
+			item.addEventListener('dragstart', () => {
+				draggedItem = item;
+				setTimeout(() => {
+					item.style.display = 'none';
+				}, 0);
+			});
+		item.addEventListener('dragend', () => {
+						setTimeout(() => {
+							draggedItem.style.display = 'flex';
+							draggedItem = null;
+						}, 0);
+					});
 				
-	<div id="yes-drop" class="drag-drop dragitem"> #yes-drop </div>
-	
-	<div  class="dropzone">
-		#outer-dropzone
-		<div  class="dropzone">#inner-dropzone</div>
-	</div>`;
 
-
-// 	// drag and drop inplement
-// 	function dragAndDrop(){
-
-		
-// 		const dragItems = document.querySelectorAll('.dragitem');
-// 		const category = document.querySelectorAll('.dropcategory');
-	
-// 		let draggedItem = null;
-		
-// 		for (let i = 0; i < dragItems.length; i++) {
-// 			const item = dragItems[i];		
-// 			item.addEventListener('dragstart', () => {
-// 				draggedItem = item;
-// 				setTimeout(() => {
-// 					item.style.display = 'none';
-// 				}, 0);
-// 			});
-// 		item.addEventListener('dragend', () => {
-// 						setTimeout(() => {
-// 							draggedItem.style.display = 'flex';
-// 							draggedItem = null;
-// 						}, 0);
-// 					});
 					
-// 					for (let j = 0; j < category.length; j++) {
-// 						const list = category[j];
+					for (let j = 0; j < category.length; j++) {
+						const list = category[j];
 						
-// 						list.addEventListener('dragover', (e) => {
-// 							e.preventDefault();
-// 						});
+						list.addEventListener('dragover', (e) => {
+							e.preventDefault();
+						});
 						
-// 						list.addEventListener('dragenter', (e) => {
-// 							e.preventDefault();
-// 						});
+						list.addEventListener('dragenter', (e) => {
+							e.preventDefault();
+						});
 						
-// 						list.addEventListener('dragleave', (e) => {
-// 							// console.log(e);
-// 							// console.log(item.id);
-// 							// console.log(this);
-// 						});
+						list.addEventListener('dragleave', (e) => {
+							// console.log(e);
+							// console.log(item.id);
+							// console.log(this);
+						});
 						
-// 						list.addEventListener('drop', function (e) {
-// 							console.log(draggedItem.id);
+						list.addEventListener('drop', function (e) {
+							console.log(draggedItem.id);
 							
-// 							console.log('drop');
-// 							// console.log(e);
-// 							// console.log(this.closest('div'));
-// 							// console.log(draggedItem.closest('dropcategory'));
-// 		// console.log(e.value);
-// 		console.log(this);
-// 		let thisBox = this;
-// 		let thisTask = draggedItem;
+							console.log('drop');
+							// console.log(e);
+							// console.log(this.closest('div'));
+							// console.log(draggedItem.closest('dropcategory'));
+		// console.log(e.value);
+		console.log(this.id);
+		console.log(draggedItem.id)
+		let thisBox = this.id;
+		let thisTask = draggedItem.id;
 
-// 		console.log(thisTask);
-// 		this.append(draggedItem);
-
-// 		const position = [{
-// 		div: thisBox,
-// 		task: thisTask
-// 		}]
+		console.log(thisTask);
 	
-// 	;
-// 	//   todoInput.value = '';
-// 	Position.push(position);
-// 	console.log(Position);
-// 	localStorage.setItem('position', JSON.stringify(Position));
+
+		 newList = [{
+
+			task: thisTask,
+			div: thisBox
+		}
+	]
+	;
+	//   todoInput.value = '';
+	PositionList.push(newList);
+	console.log(PositionList);
+	
+	localStorage.setItem('Position', JSON.stringify(PositionList));
+		
+}
+);
+	
+	
+	//  PositionList = JSON.parse(localStorage.getItem('Position'));
+	//  console.log(Position)
+
+	// Position.indexOf([position]) === -1 ? Position.push(position) : console.log("This item already exists");
 
 
-//     });
-//   }
+
+
+  }
+}
+	}
+dragAndDrop();
+let CategoryArray = [];
+
+function setItemInBox(){
+
+	let boxes = document.getElementsByClassName('box');
+// 	for(var i = 0; i < boxes.length; i++)
+// {
+//    console.log(boxes[i].id);
+//    let item = boxes[i].id;
+// //    let Pushh = item.push(CategoryArray)
+
+// //    let categorynode = boxes.id;
+// console.log(divsArr);
+
 // }
-// }
-// dragAndDrop();
+const divsArr = Array.from(boxes);
+// const divsArr = Array.from(item);
+// console.log(divsArr[1].id);
+divsArr.forEach((item)=>{
 
+console.log(item.id)
+item.childNodes.forEach(item => ( console.log(item)
+
+));
+
+})
+
+
+	//! dla każdego elementu children sprawdź jego chi.klasa 
+	
+	 PositionList = JSON.parse(localStorage.getItem('Position'));
+	 console.log(PositionList);
+
+var oldParent = document.querySelector('.categoryTodos');
+var newParent = document.getElementById('ToDo');
+
+console.log(oldParent);
+console.log(oldParent);
+
+var children = oldParent.childNodes;
+// var children = Array.prototype.slice.call(children);
+console.log(children);
+
+
+console.log(newParent);
+while (oldParent.childNodes.length > 0) { 
+	
+	//! jeżeli nazwa kategori rodzica nie równa się kategori z node.value dziecka wtedy dodaj (appen?) dziecko do rodzica którego kategoria jest równa node.value
+	
+    newParent.appendChild(oldParent.childNodes[0]);
+}
+
+}
+setItemInBox();
 
 
 
@@ -365,7 +449,7 @@ function renderoptionCategory(){
 		console.log(categories);
 	taskCategory.innerHTML = categories.map(
 		(category) =>`
-					<option value="${category}">${category.text}</option>
+					<option value="${category.text}">${category.text}</option>
 					`,
 		)
 		.join('');
@@ -373,95 +457,3 @@ function renderoptionCategory(){
 
 }
 
-
-
-
-/* The dragging code for '.draggable' from the demo above
- * applies to this demo as well so it doesn't have to be repeated. */
-const dropzone = document.querySelectorAll('dropzone')
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '.dragitem',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
-
-  // listen for drop related events:
-
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active')
-  },
-//   ondragenter: function (event) {
-//     var draggableElement = event.relatedTarget
-// 	// dropzone.append(draggableElement);
-// 	console.log(draggableElement);
-// 	console.log(dropzone)
-
-//     var dropzoneElement = event.target
-
-//     // feedback the possibility of a drop
-//     dropzoneElement.classList.add('drop-target')
-//     draggableElement.classList.add('can-drop')
-
-//     draggableElement.textContent = 'Dragged in'
-//   },
-  ondragleave: function (event) {
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-    event.relatedTarget.textContent = 'Dragged out'
-  },
-  ondrop: function (event) {
-    event.relatedTarget.textContent = 'Dropped'
-  },
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
-  }
-})
-
-interact('.drag-drop')
-  .draggable({
-    inertia: true,
-    modifiers: [
-      interact.modifiers.restrictRect({
-        restriction: 'parent',
-        endOnly: true
-      })
-    ],
-    autoScroll: true,
-    // dragMoveListener from the dragging demo above
-    listeners: { move: dragMoveListener }
-  })
-
-
-  function dragMoveListener (event) {
-  var target = event.target
-  // keep the dragged position in the data-x/data-y attributes
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-  // translate the element
-  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-  // update the posiion attributes
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
-}
-
-// this function is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener
-
-let out = document.createElement('div');
-out.className ='dropzone';
-newtoDoBox.appendChild(out);
-let inner = document.createElement('div')
-inner.className ='dropzone';
-out.appendChild(inner);
-
-{/* <div id="outer-dropzone" class="dropzone">
-		#outer-dropzone
-		<div id="inner-dropzone" class="dropzone">#inner-dropzone</div>
-	</div>`; */}
