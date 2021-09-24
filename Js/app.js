56f59de35484874f87195323792435facec1e5f3
bartlomiejra <bartlomiej.ra@gmail.com>
2021-09-24 15:17:38 +0200

    push

--- Js/app.js
+++ Js/app.js
@@ -1004,3 +1004,25 @@ console.log("oki");
         .join('');
 }
 titleName();
+const button = document.getElementById('notifications');
+button.addEventListener('click', () => {
+  Notification.requestPermission().then((result) => {
+    if (result === 'granted') {
+      randomNotification();
+    }
+  });
+})
+
+
+function randomNotification() {
+  const randomItem = Math.floor(Math.random() * games.length);
+  const notifTitle = games[randomItem].name;
+  const notifBody = `Created by ${games[randomItem].author}.`;
+  const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+  new Notification(notifTitle, options);
+  setTimeout(randomNotification, 30000);
+}
