de3a9cde3f0cb3556b47ff7138450997c27bea6f
bartlomiejra <bartlomiej.ra@gmail.com>
2021-11-18 14:57:12 +0100

    auth firebase

--- public/Js/app.js
+++ public/Js/app.js
@@ -758,37 +758,4 @@ export {
 
 
 
-///// User Authentication /////
 
-const auth = firebase.auth();
-
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
-
-const signInBtn = document.getElementById('signInBtn');
-const signOutBtn = document.getElementById('signOutBtn');
-
-const userDetails = document.getElementById('userDetails');
-
-
-const provider = new firebase.auth.GoogleAuthProvider();
-
-/// Sign in event handlers
-
-signInBtn.onclick = () => auth.signInWithPopup(provider);
-
-signOutBtn.onclick = () => auth.signOut();
-
-auth.onAuthStateChanged(user => {
-    if (user) {
-        // signed in
-        whenSignedIn.hidden = false;
-        whenSignedOut.hidden = true;
-        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
-    } else {
-        // not signed in
-        whenSignedIn.hidden = true;
-        whenSignedOut.hidden = false;
-        userDetails.innerHTML = '';
-    }
-});
