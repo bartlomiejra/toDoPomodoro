d5328a0c8e05435d16098ad6965168000e818ee4
bartlomiejra <bartlomiej.ra@gmail.com>
2021-11-21 17:49:56 +0100

    update

--- public/Js/app.js
+++ public/Js/app.js
@@ -1,5 +1,37 @@
 /* eslint-disable prettier/prettier */
+   const auth = firebase.auth();
 
+    const whenSignedIn = document.getElementById('whenSignedIn');
+    const whenSignedOut = document.getElementById('whenSignedOut');
+
+    const signInBtn = document.getElementById('signInBtn');
+    const signOutBtn = document.getElementById('signOutBtn');
+
+    const userDetails = document.getElementById('userDetails');
+
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    /// Sign in event handlers
+
+    console.log(signInBtn)
+    signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+    signOutBtn.onclick = () => auth.signOut();
+
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<p>Hi ${user.displayName}!</p>`;
+      } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+      }
+    }); 
 import {
   showToDoCard,
   showProjectList,
