2e0dad2610d99ae843da22531934307c02d4f5e2
bartlomiejra <bartlomiej.ra@gmail.com>
2021-12-09 19:16:43 +0100

    firebase update

--- public/Js/settings.js
+++ public/Js/settings.js
@@ -39,76 +39,53 @@ thingsRef = db.collection("users");
 auth.onAuthStateChanged((user) => {
   if (user) {
     logUserId = user.uid;
-    // get all data from firebase and console.log it
-    // const alldata = [];
-    // thingsRef.doc(user.uid).collection("settings").doc("0")
-    //   .get()
-    //   .then((doc) => {
-    //             console.log("Document data:", doc.data());
-    //           });
-
-    //   snapshot.docs.forEach((docs) => {
-    //     console.log(docs.data());
-    // //     alldata.push(doc.data());
-    //   });
-    // console.log(alldata);
-
-    //     firebaseSettings = alldata[0].ListTodo;
-    //     console.log(firebaseSettings);
-    //   });
-
-    // db.collection("users")
-    // var messageRef = db.collection('users').doc('roomA')
-    //             .collection('messages').doc('message1');
-
-    // console.log(thingsRef);
+   
     saveSettings.onclick = (event) => {
       console.log("ok");
       event.preventDefault();
-
-      // const { serverTimestamp } = firebase.firestore.FieldValue;
       thingsRef.doc(user.uid).collection("settings").doc("0").update({
-        Sound,
+        Sound: true,
         Theme: themeselected,
         pomodoreTime: pomodoreTime.value,
         breakTime: breakTimeValue.value,
       });
     };
-let myArray;
+let firestoreTheme;
+let firestorepomodoreTime;
+let firestoreBreakTime;
+let firestoreSound;
     // Query
-    // .where("uid", "!=", user.uid)
+ 
     unsubscribe = thingsRef
       .doc(logUserId)
       .collection("settings")
       .onSnapshot((querySnapshot) => {
         // Map results to an array of li elements
-
+        const elements = querySnapshot.docs;
         const items = querySnapshot.docs.map((doc) => {
-          
-          console.log(doc);
-          return `<li>${doc.data().Sound},${doc.data().Theme},${doc.data().breakTime},</li>`;
-          console.log(sounds)
-        });
-        console.log(items);
-        console.log(myArray);
-        // console.log(doc.data().pomodoreTime);
 
+          
+          firestoreTheme = doc.data().Theme;
+          firestorepomodoreTime = doc.data().pomodoreTime;
+          firestoreBreakTime = doc.data().breakTime;
+          firestoreSound = doc.data().Sound;
+                  });
+        
         thingsList.innerHTML = items.join("");
-      });
-    theme.innerHTML = `
+        theme.innerHTML = `
 <div class="switch-button">
-  <!-- doc.data.Theme == "Dark" ? mo ze taja jibstryjcha  -->
-  settingUser.Theme} == "Dark"
-      ? '<input class="black switch-button-checkbox" type="checkbox"></input>'
-      : '<input class="black switch-button-checkbox" checked type="checkbox"></input>'
+  
+  ${
+    firestoreTheme == "Dark"    
+    ? '<input class="black switch-button-checkbox" type="checkbox"></input>'
+    : '<input class="black switch-button-checkbox" checked type="checkbox"></input>'
   }
+
 <label class="switch-button-label" for=""><span class="switch-button-label-span">Dark</span></label>
+
 </div>
 `;
-  } else {
-    unsubscribe && unsubscribe();
-  }
-});
+
     SetTimes.innerHTML = `
   <li class="duration optionsBox"  >
                 <label for="quantity">Pomodore duration:</label>
@@ -122,7 +99,7 @@ let myArray;
                   min="5"
                   max="100"
 				  />
-				  <output name="duration"  class="durationTime" id="quantity">${settingUser.pomodoreTime}</output>
+				  <output name="duration"  class="durationTime" id="quantity">${firestorepomodoreTime}</output>
 				  </div>
 				  </li>
 				  <li class="duration optionsBox">
@@ -141,11 +118,15 @@ let myArray;
 		min="1"
 		max="60"
 		/>
-		<output name="break"  id="quantitybreak" class="breakTime" >${settingUser.breakTime} </output>
+		<output name="break"  id="quantitybreak" class="breakTime" >${firestoreBreakTime} </output>
 		</div>            
               </li>
 			  `;
-
+});
+  } else {
+    unsubscribe && unsubscribe();
+  }
+});
 console.log(logUserId);
 export function closeModal(modal) {
   if (modal == null) return;
@@ -177,13 +158,13 @@ const breakTimeValue = document.getElementById("quantitybreak");
 export function saveOptions() {
   event.preventDefault();
   themesValue();
-  const settings = {
-    Theme: themeselected,
-    Sound: settingUser.Sound,
-    pomodoreTime: pomodoreTime.value,
-    breakTime: breakTimeValue.value,
-  };
-  window.localStorage.setItem("settings", JSON.stringify(settings));
+  // const settings = {
+  //   Theme: themeselected,
+  //   Sound: settingUser.Sound,
+  //   pomodoreTime: pomodoreTime.value,
+  //   breakTime: breakTimeValue.value,
+  // };
+  // window.localStorage.setItem("settings", JSON.stringify(settings));
 }
 saveSettings.addEventListener("click", saveOptions);
 let Sound = true;
